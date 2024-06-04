5a45ae0f39d428a292f3a338326df1687b93a5a4
Ham-Seung-Wan <syham001@naver.com>
2024-06-04 12:02:07 +0900

    init commit

--- src/main/java/com/prac/music/domain/board/entity/Board.java
+++ src/main/java/com/prac/music/domain/board/entity/Board.java
@@ -1,13 +1,27 @@
 package com.prac.music.domain.board.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.prac.music.domain.user.entity.User;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "board")
 public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, name = "contents")
+    private String contents;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn( nullable = false,name = "user_id")
+    private User user;
+
+    @Column(nullable = false, name = "createdAt")
+    private LocalDateTime createAt;
+
+    @Column(nullable = false, name = "updatedAt")
+    private LocalDateTime updatedAt;
 }
