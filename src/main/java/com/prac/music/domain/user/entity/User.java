35021c7b441b14da9e409b7886f8a7b235468e4a
이세비 <lsy205105@naver.com>
2024-06-04 20:03:24 +0900

    ✨ update : security & jwt

--- src/main/java/com/prac/music/domain/user/entity/User.java
+++ src/main/java/com/prac/music/domain/user/entity/User.java
@@ -1,15 +1,23 @@
 package com.prac.music.domain.user.entity;
 
+import com.prac.music.domain.user.dto.SignupRequestDto;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
 @Table(name = "user")
-public class User {
+@NoArgsConstructor
+public class User extends BaseTimeEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true, nullable = false, name = "userid")
+    private String userId;
 
     @Column(unique = true, nullable = false, name = "email")
     private String email;
@@ -29,12 +37,10 @@ public class User {
     @Column(name = "refresh_token")
     private String refreshToken;
 
-    @Column(nullable = false, name = "createdAt")
-    private LocalDateTime createAt;
-
-    @Column(nullable = false, name = "updatedAt")
-    private LocalDateTime updatedAt;
-
-    @Column(nullable = false, name = "deletedAt")
-    private LocalDateTime deletedAt;
+    public User(SignupRequestDto requestDto) {
+        this.userId = requestDto.getUserId();
+        this.name = requestDto.getName();
+        this.intro = requestDto.getIntro();
+        this.userStatusEnum = UserStatusEnum.NORMAL;
+    }
 }
