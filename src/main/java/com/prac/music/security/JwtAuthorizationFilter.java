a9cd920b1180a2e5e780fe259654d05bb21a60e5
2minus <blecker0610@gmail.com>
2024-06-07 07:57:29 +0900

    ✨update : JwtAuthorizationFilter 인가 진행 중 user의 status 확인 추가, user에 저장된 refreshtoken과 Httprequest의 refreshtoken 비교 과정 추가

--- src/main/java/com/prac/music/security/JwtAuthorizationFilter.java
+++ src/main/java/com/prac/music/security/JwtAuthorizationFilter.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import com.prac.music.domain.user.entity.User;
 import com.prac.music.domain.user.repository.UserRepository;
 import com.prac.music.domain.user.service.JwtService;
-import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
@@ -56,26 +55,38 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
                     () -> new IllegalArgumentException("사용자를 찾을 수 없습니다.")
             );
 
-            String refreshToken = user.getRefreshToken();
-            log.info("Refresh Token: " + refreshToken);
+            if(!user.isExist()){
+                throw new IllegalStateException("탈퇴된 사용자입니다.");
+            }
 
-            // 토큰이 만료된 경우
-            if (jwtService.isTokenExpired(tokenValue)) {
-                // 리프레시 토큰이 유효한 경우, 새로운 토큰 생성
-                if (!jwtService.isRefreshTokenExpired(refreshToken)) {
-                    jwtService.createToken(user.getUserId());
-                    System.out.println("jwtService.createToken(user.getUserId()) = " + jwtService.createToken(user.getUserId()));
-                    jwtService.createRefreshToken(user.getUserId());
-                    log.info("새로운 토큰이 생성되었습니다.");
-                    setAuthentication(claims.getSubject());
-                    filterChain.doFilter(req, res);
+            String userRefreshToken = jwtService.substringToken(user.getRefreshToken());
+            String refreshToken = jwtService.getRefreshJwtFromHeader(req);
+
+            log.info("User's Refresh Token: " + userRefreshToken);
+            log.info("response's Refresh Token: " + refreshToken);
+
+            // 유저가 보유한 토큰과 httpservlet의 토큰이 같은지 비교
+            if (userRefreshToken.equals(refreshToken)) {
+                // 토큰이 만료된 경우
+                if (jwtService.isTokenExpired(tokenValue)) {
+                    // 리프레시 토큰이 유효한 경우, 새로운 토큰 생성
+                    if (!jwtService.isRefreshTokenExpired(userRefreshToken)) {
+                        jwtService.createToken(user.getUserId());
+                        System.out.println("jwtService.createToken(user.getUserId()) = " + jwtService.createToken(user.getUserId()));
+                        jwtService.createRefreshToken(user.getUserId());
+                        log.info("새로운 토큰이 생성되었습니다.");
+                        setAuthentication(claims.getSubject());
+                        filterChain.doFilter(req, res);
+                    } else {
+                        // 리프레시 토큰도 만료된 경우
+                        throw new IllegalArgumentException("다시 재로그인해주세요");
+                    }
                 } else {
-                    // 리프레시 토큰도 만료된 경우
-                    throw new IllegalArgumentException("다시 재로그인해주세요");
+                    // 토큰이 유효한 경우
+                    setAuthentication(claims.getSubject());
                 }
             } else {
-                // 토큰이 유효한 경우
-                setAuthentication(claims.getSubject());
+                throw new IllegalArgumentException("토큰의 정보가 일치하지 않습니다.");
             }
         }
 
