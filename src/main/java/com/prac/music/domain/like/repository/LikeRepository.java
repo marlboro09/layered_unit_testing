bb28dd0f5076b983183893743264e8c6f10be614
hgi4657 <hgi4657@naver.com>
2024-06-08 21:18:04 +0900

    🔥 del  : LikeRepository 삭제

--- src/main/java/com/prac/music/domain/like/repository/LikeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.prac.music.domain.like.repository;
-
-import com.prac.music.domain.like.entity.Like;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LikeRepository extends JpaRepository<Like, Long> {
-}
