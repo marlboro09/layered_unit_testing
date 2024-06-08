d06c9fea5df4a5f67b4e9557e96ee8359bc7f4a7
hgi4657 <hgi4657@naver.com>
2024-06-08 22:55:05 +0900

    🩹 fix : 데이터 저장 시 contentType 추가

--- src/main/java/com/prac/music/domain/board/entity/BoardLike.java
+++ src/main/java/com/prac/music/domain/board/entity/BoardLike.java
@@ -1,13 +1,17 @@
 package com.prac.music.domain.board.entity;
 
+import com.prac.music.domain.like.entity.ContentType;
 import com.prac.music.domain.like.entity.Like;
+import com.prac.music.domain.user.entity.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "board_like")
+@NoArgsConstructor
 public class BoardLike extends Like {
     @ManyToOne
     @JoinColumn(name = "board_id", nullable = false)
@@ -16,5 +20,11 @@ public class BoardLike extends Like {
     @Override
     public Long getContentId() {
         return board.getId();
+    }
+
+    public BoardLike(Board board, User user) {
+        this.board = board;
+        this.user = user;
+        this.contentType = ContentType.BOARD;
     }
 }
