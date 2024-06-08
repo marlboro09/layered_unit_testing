0a2fa261b39c96e1962846814dfae4fcbab78ce0
hgi4657 <hgi4657@naver.com>
2024-06-08 23:00:38 +0900

    ✨ update : 좋아요 Controller 추가 - 반환 시 해당 ID에 대한 모든 좋아요 출력

--- src/main/java/com/prac/music/domain/like/controller/LikeController.java
+++ src/main/java/com/prac/music/domain/like/controller/LikeController.java
@@ -3,6 +3,7 @@ package com.prac.music.domain.like.controller;
 import com.prac.music.domain.like.service.LikeService;
 import com.prac.music.domain.user.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,12 +18,14 @@ public class LikeController {
     private final LikeService likeService;
 
     @PostMapping("/like")
-    public void boardLike(@PathVariable Long boardId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-
+    public ResponseEntity<String> boardLike(@PathVariable Long boardId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        int boardAllLike = likeService.boardLike(boardId, userDetails.getUser());
+        return ResponseEntity.ok("해당 게시글의 전체 좋아요 : " + boardAllLike);
     }
 
     @PostMapping("/comments/{commentId}/like")
-    public void commentLike(@PathVariable Long boardId, @PathVariable Long commentId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-
+    public ResponseEntity<String> commentLike(@PathVariable Long boardId, @PathVariable Long commentId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        int commentAllLike = likeService.commentLike(boardId, commentId, userDetails.getUser());
+        return ResponseEntity.ok("해당 댓글의 전체 좋아요 : " + commentAllLike);
     }
 }
