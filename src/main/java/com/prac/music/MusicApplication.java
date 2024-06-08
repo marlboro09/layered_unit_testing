bb8c810de3c939a1f80cfdb24cbcdefd709a5b78
Ham-Seung-Wan <syham001@naver.com>
2024-06-08 17:25:29 +0900

    🩹 fix : @EnableJpaAuditing은 필요함 BaseTimeEntity때문에

--- src/main/java/com/prac/music/MusicApplication.java
+++ src/main/java/com/prac/music/MusicApplication.java
@@ -7,9 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class MusicApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MusicApplication.class, args);
     }
-
 }
