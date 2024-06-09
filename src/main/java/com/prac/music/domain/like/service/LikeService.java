1207e55a0104e516f9c1b7d6df57c278474d18a3
hgi4657 <hgi4657@naver.com>
2024-06-09 20:35:07 +0900

    🩹 fix : Board 관련 메서드 접근 제어자 변경

--- src/main/java/com/prac/music/domain/like/service/LikeService.java
+++ src/main/java/com/prac/music/domain/like/service/LikeService.java
@@ -3,6 +3,7 @@ package com.prac.music.domain.like.service;
 import com.prac.music.domain.board.entity.Board;
 import com.prac.music.domain.board.entity.BoardLike;
 import com.prac.music.domain.board.repository.BoardRepository;
+import com.prac.music.domain.board.service.BoardService;
 import com.prac.music.domain.comment.entity.Comment;
 import com.prac.music.domain.comment.entity.CommentLike;
 import com.prac.music.domain.comment.repository.CommentRepository;
@@ -22,15 +23,13 @@ public class LikeService {
 
     private final BoardLikeRepository boardLikeRepository;
     private final CommentLikeRepository commentLikeRepository;
-    private final BoardRepository boardRepository;
     private final CommentRepository commentRepository;
+    private final BoardService boardService;
     private final ProfileService profileService;
 
     @Transactional
     public int boardLike(Long boardId, User user) {
-        Board board = boardRepository.findById(boardId).orElseThrow(() ->
-                new IllegalArgumentException("해당 게시판 존재하지 않는다")
-        );
+        Board board = boardService.findBoardById(boardId);
         User getUser = profileService.findUserById(user.getUserId());
 
         if (board.getUser().getId().equals(getUser.getId())) {
@@ -52,9 +51,7 @@ public class LikeService {
 
     @Transactional
     public int commentLike(Long boardId, Long commentId, User user) {
-        Board board = boardRepository.findById(boardId).orElseThrow(() ->
-                new IllegalArgumentException("해당 게시판 존재하지 않는다")
-        );
+        boardService.findBoardById(boardId);
         User getUser = profileService.findUserById(user.getUserId());
         Comment comment = commentRepository.findById(commentId).orElseThrow(() ->
                 new IllegalArgumentException("해당 댓글은 존재하지 않는다")
