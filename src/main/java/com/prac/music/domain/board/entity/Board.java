d1262977f16b08068b5db10718b2027f17f5b416
marlboro09 <taebong8989@gmail.com>
2024-06-10 17:53:56 +0900

    🩹 fix : 게시판 paging param으로 페이지를 입력 받게 수정함

--- src/main/java/com/prac/music/domain/board/entity/Board.java
+++ src/main/java/com/prac/music/domain/board/entity/Board.java
@@ -1,33 +1,52 @@
 package com.prac.music.domain.board.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.prac.music.domain.comment.entity.Comment;
 import com.prac.music.domain.user.entity.BaseTimeEntity;
 import com.prac.music.domain.user.entity.User;
 
-import jakarta.persistence.*;
-import lombok.*;
-
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자는 protected로 설정합니다.
 @Entity
 @Table(name = "board")
 public class Board extends BaseTimeEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(nullable = false, name = "title")
-    private String title;
+	@Column(nullable = false, name = "title")
+	private String title;
 
-    @Column(nullable = false, name = "contents")
-    private String contents;
+	@Column(nullable = false, name = "contents")
+	private String contents;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(nullable = false, name = "user_id")
-    private User user;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(nullable = false, name = "user_id")
+	private User user;
 
 	@OneToMany(mappedBy = "board", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
 	private List<Comment> comments;
+
+	@OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<BoardFiles> boardFiles = new ArrayList<>();
 
 	@Builder
 	public Board(String title, String contents, User user) {
@@ -35,19 +54,9 @@ public class Board extends BaseTimeEntity {
 		this.contents = contents;
 		this.user = user;
 	}
-    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<BoardFiles> boardFiles = new ArrayList<>();
 
-
-    @Builder
-    public Board(String title, String contents, User user) {
-        this.title = title;
-        this.contents = contents;
-        this.user = user;
-    }
-
-    public void update(String title, String contents) {
-        this.title = title;
-        this.contents = contents;
-    }
+	public void update(String title, String contents) {
+		this.title = title;
+		this.contents = contents;
+	}
 }
--- src/main/java/com/prac/music/domain/board/service/BoardService.java
+++ src/main/java/com/prac/music/domain/board/service/BoardService.java
@@ -1,6 +1,17 @@
 package com.prac.music.domain.board.service;
 
-import com.prac.music.common.exception.BoardNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.prac.music.common.exception.BoardServiceException;
 import com.prac.music.common.exception.UnauthorizedAccessException;
 import com.prac.music.common.service.S3Service;
 import com.prac.music.domain.board.dto.BoardRequestDto;
@@ -12,110 +23,101 @@ import com.prac.music.domain.board.repository.BoardFilesRepository;
 import com.prac.music.domain.board.repository.BoardRepository;
 import com.prac.music.domain.user.entity.User;
 import com.prac.music.domain.user.repository.UserRepository;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class BoardService {
 
-    private final BoardRepository boardRepository;
-    private final UserRepository userRepository;
-    private final BoardFilesRepository boardFilesRepository;
-    private final S3Service s3Service;
+	private final BoardRepository boardRepository;
+	private final UserRepository userRepository;
+	private final BoardFilesRepository boardFilesRepository;
+	private final S3Service s3Service;
 
-    @Transactional
-    public BoardResponseDto createBoard(BoardRequestDto requestDto,
-                                        User user, List<MultipartFile> files) throws IOException {
-        User persistentUser = findUserById(user.getId());
-        Board board = Board.builder()
-                .title(requestDto.getTitle())
-                .contents(requestDto.getContents())
-                .user(persistentUser)
-                .build();
-        boardRepository.save(board);
+	@Transactional
+	public BoardResponseDto createBoard(BoardRequestDto requestDto,
+		User user, List<MultipartFile> files) throws IOException {
+		User persistentUser = findUserById(user.getId());
+		Board board = Board.builder()
+			.title(requestDto.getTitle())
+			.contents(requestDto.getContents())
+			.user(persistentUser)
+			.build();
+		boardRepository.save(board);
 
-        if (files != null && !files.isEmpty()) {
-            if (files.size() > 5) {
-                throw new IllegalArgumentException("파일은 5개까지 올릴 수 있습니다.");
-            }
+		if (files != null && !files.isEmpty()) {
+			if (files.size() > 5) {
+				throw new IllegalArgumentException("파일은 5개까지 올릴 수 있습니다.");
+			}
 
-            List<BoardFiles> boardFiles = new ArrayList<>();
+			List<BoardFiles> boardFiles = new ArrayList<>();
 
-            for (MultipartFile file : files) {
-                String fileUrl = s3Service.s3Upload(file);
+			for (MultipartFile file : files) {
+				String fileUrl = s3Service.s3Upload(file);
 
-                BoardFiles boardFile = BoardFiles.builder()
-                        .file(fileUrl)
-                        .board(board)
-                        .build();
-                boardFiles.add(boardFile);
-            }
-            boardFilesRepository.saveAll(boardFiles);
-        }
-        return new BoardResponseDto(board);
-    }
+				BoardFiles boardFile = BoardFiles.builder()
+					.file(fileUrl)
+					.board(board)
+					.build();
+				boardFiles.add(boardFile);
+			}
+			boardFilesRepository.saveAll(boardFiles);
+		}
+		return new BoardResponseDto(board);
+	}
 
-    @Transactional
-    public BoardResponseDto updateBoard(Long id,
-                                        UpdateRequestDto requestDto,
-                                        User user, List<MultipartFile> files) throws IOException {
-        Board board = findBoardById(id);
-        User persistentUser = findUserById(user.getId());
+	@Transactional
+	public BoardResponseDto updateBoard(Long id,
+		UpdateRequestDto requestDto,
+		User user, List<MultipartFile> files) throws IOException {
+		Board board = findBoardById(id);
+		User persistentUser = findUserById(user.getId());
 
-        validateUserAuthorization(board, persistentUser);
+		validateUserAuthorization(board, persistentUser);
 
-        if (files != null && !files.isEmpty()) {
-            if (files.size() > 5) {
-                throw new IllegalArgumentException("파일은 5개까지 올릴 수 있습니다.");
-            }
+		if (files != null && !files.isEmpty()) {
+			if (files.size() > 5) {
+				throw new IllegalArgumentException("파일은 5개까지 올릴 수 있습니다.");
+			}
 
-            List<BoardFiles> boardFiles = new ArrayList<>();
+			List<BoardFiles> boardFiles = new ArrayList<>();
 
-            for (MultipartFile file : files) {
-                String fileUrl = s3Service.s3Upload(file);
+			for (MultipartFile file : files) {
+				String fileUrl = s3Service.s3Upload(file);
 
-                BoardFiles boardFile = BoardFiles.builder()
-                        .file(fileUrl)
-                        .board(board)
-                        .build();
-                boardFiles.add(boardFile);
-            }
-            boardFilesRepository.saveAll(boardFiles);
-        }
+				BoardFiles boardFile = BoardFiles.builder()
+					.file(fileUrl)
+					.board(board)
+					.build();
+				boardFiles.add(boardFile);
+			}
+			boardFilesRepository.saveAll(boardFiles);
+		}
 
-        board.update(requestDto.getTitle(), requestDto.getContents());
-        Board updatedBoard = boardRepository.save(board);
-        return new BoardResponseDto(updatedBoard);
-    }
+		board.update(requestDto.getTitle(), requestDto.getContents());
+		Board updatedBoard = boardRepository.save(board);
+		return new BoardResponseDto(updatedBoard);
+	}
 
-    @Transactional
-    public void deleteBoard(Long id, User user) {
-        Board board = findBoardById(id);
-        User persistentUser = findUserById(user.getId());
+	@Transactional
+	public void deleteBoard(Long id, User user) {
+		Board board = findBoardById(id);
+		User persistentUser = findUserById(user.getId());
 
-        validateUserAuthorization(board, persistentUser);
+		validateUserAuthorization(board, persistentUser);
 
-        boardRepository.delete(board);
-    }
+		boardRepository.delete(board);
+	}
 
-    @Transactional(readOnly = true)
-    public List<BoardResponseDto> getAllBoard() {
-        return boardRepository.findAll().stream()
-                .map(BoardResponseDto::new)
-                .collect(Collectors.toList());
-    }
+	@Transactional(readOnly = true)
+	public List<BoardResponseDto> getAllBoard() {
+		return boardRepository.findAll().stream()
+			.map(BoardResponseDto::new)
+			.collect(Collectors.toList());
+	}
 
 	@Transactional(readOnly = true)
 	public Page<BoardResponseDto> paging(Pageable pageable) {
@@ -123,10 +125,6 @@ public class BoardService {
 		return boards.map(BoardResponseDto::new);
 	}
 
-    private User findUserById(Long userId) {
-        return userRepository.findById(userId)
-                .orElseThrow(() -> new BoardNotFoundException("사용자 ID " + userId + "를 찾을 수 없습니다."));
-    }
 	@Transactional(readOnly = true)
 	public BoardResponseDto getBoardById(Long id) {
 		Board board = findBoardByIdWithComments(id);
@@ -135,22 +133,22 @@ public class BoardService {
 
 	private User findUserById(Long userId) {
 		return userRepository.findById(userId)
-			.orElseThrow(() -> new BoardNotFoundException("사용자 ID " + userId + "를 찾을 수 없습니다."));
+			.orElseThrow(() -> new BoardServiceException("사용자 ID " + userId + "를 찾을 수 없습니다."));
 	}
 
 	public Board findBoardById(Long boardId) {
 		return boardRepository.findById(boardId)
-			.orElseThrow(() -> new BoardNotFoundException("게시물 ID " + boardId + "를 찾을 수 없습니다."));
+			.orElseThrow(() -> new BoardServiceException("게시물 ID " + boardId + "를 찾을 수 없습니다."));
 	}
 
 	private Board findBoardByIdWithComments(Long boardId) {
 		return boardRepository.findByIdWithComments(boardId)
-			.orElseThrow(() -> new BoardNotFoundException("게시물 ID " + boardId + "를 찾을 수 없습니다."));
+			.orElseThrow(() -> new BoardServiceException("게시물 ID " + boardId + "를 찾을 수 없습니다."));
 	}
 
-    private void validateUserAuthorization(Board board, User user) {
-        if (!board.getUser().equals(user)) {
-            throw new UnauthorizedAccessException("권한이 없습니다.");
-        }
-    }
+	private void validateUserAuthorization(Board board, User user) {
+		if (!board.getUser().equals(user)) {
+			throw new UnauthorizedAccessException("권한이 없습니다.");
+		}
+	}
 }
