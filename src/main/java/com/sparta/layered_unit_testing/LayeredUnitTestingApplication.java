2a3f2b0ac2138ba5b8e377a257054ca566d850bc
marlboro09 <taebong8989@gmail.com>
2024-06-14 21:49:04 +0900

    init commit

---
+++ src/main/java/com/sparta/layered_unit_testing/LayeredUnitTestingApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.layered_unit_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayeredUnitTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayeredUnitTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/layered_unit_testing/LayeredUnitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.layered_unit_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayeredUnitTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
